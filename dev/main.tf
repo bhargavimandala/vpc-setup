02106511433ed90e2ce17ce33b51943e892bc324
Bhargavi <bhargavimandala@gmail.com>
2022-11-20 11:28:54 +0000

    vpc setup in dev environment

---
+++ dev/main.tf
@@ -0,0 +1,24 @@
+module "my_vpc" {
+    source = "git@github.com:bhargavimandala/aws-vpc-module-terraform.git"
+    cidr_block = "10.0.0.0/16"
+    product = "tesla"
+    environment = "dev"
+    publicsubnet_cidr = "10.0.1.0/28,10.0.2.0/28,10.0.5.0/28"
+    public_az = "eu-west-2a,eu-west-2b,eu-west-2c"
+    privatesubnet_cidr = "10.0.3.0/28,10.0.4.0/28,10.0.6.0/28"
+    private_az = "eu-west-2c,eu-west-2a,eu-west-2b"
+
+}
+
+module "staging_vpc" {
+    source = "git@github.com:bhargavimandala/aws-vpc-module-terraform.git"
+    cidr_block = "10.15.0.0/22"
+    product = "tesla"
+    environment = "dev"
+    publicsubnet_cidr = "10.15.0.0/24,10.15.1.0/24"
+    public_az = "eu-west-2a,eu-west-2b"
+    privatesubnet_cidr = "10.15.2.0/24,10.15.3.0/24"
+    private_az = "eu-west-2c,eu-west-2a"
+
+}
+
